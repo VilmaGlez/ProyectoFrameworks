04c954636923a3a781f68ed29779f6a0c025d907
TranquilinoHG <83606375+TranquilinoHG@users.noreply.github.com>
2021-05-05 13:09:15 -0500

    Update MarcasServiceImplTest.java

--- ProyectoFrameworksServicios/src/test/java/cursoDAgil/service/marcas/MarcasServiceImplTest.java
+++ ProyectoFrameworksServicios/src/test/java/cursoDAgil/service/marcas/MarcasServiceImplTest.java
@@ -1,101 +1,85 @@
 package cursoDAgil.service.marcas;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
-
-import org.junit.Ignore;
 import org.junit.Test;
-
-
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cursoDAgil.bd.domain.Marcas;
-import cursoDAgil.service.marcas.MarcasService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/applicationContext.xml"})
-
-
+@ContextConfiguration(locations={"/applicationContext.xml"})
 public class MarcasServiceImplTest {
+	
 	@Inject
 	MarcasService marcasService;
-	
-	@Ignore
-	public void consultarMarcaPorId(){
-		Marcas marcas = new Marcas();
-		Map<String, Integer>mapMarca = new HashMap<>();
-		mapMarca.put("idMarca",5);
-		try{
-			marcas=marcasService.obtenerMarcaPorId(mapMarca);
-			assertNotNull(marcas);
-			System.out.println("\n\nTest consultar marca por id");
-			System.out.println("id: "+ marcas.getIdMarca());
-			System.out.println("nombre:"+ marcas.getNombreMarca());
-			System.out.println(" ");
-		}catch(Exception e){
-			System.out.println("Error:"+ e);
-		}
-	}
 
 	@Test
 	public void pruebaConsultarTodo(){
-		int reg;
-		System.out.println("Test consultar todas las marcas");
 		try{
-			List<Marcas> lista= marcasService.obtenerMarcas();
-			reg=lista.size();
-			assertEquals(lista.size(),reg);
-			System.out.println("\nRegistros en la tabla: "+ reg);
-		}catch(Exception ex){
-			System.out.println("Error: "+ ex);
+			System.out.println("Test consultar todas las marcas service");
+			List<Marcas> lista = marcasService.obtenerMarcas();
+			assertEquals(lista.size(),4);
+		}catch(Exception e){
+			System.out.println("error" + e);
 		}
 	}
-	
-	
-	@Ignore
-	public void nuevoRegistro(){
-		Marcas marcas = new Marcas();
+	@Test
+	public void pruebaConsultarPorId(){
+		Marcas marcas=new Marcas();
+		Map<String,Integer> mapMarca=new HashMap<>();
+		mapMarca.put("idMarca", 3);
+		System.out.println("\n\nTest consultar marca por id");
 		
-		System.out.println("Test nueva marca");
+		try{
+			marcas=marcasService.obtenerMarcaPorId(mapMarca);
+			System.out.println("id:"+marcas.getIdMarca());
+			System.out.println("nombre:"+marcas.getNombreMarca());
+			
+		}catch(Exception e){
+			System.out.println("Error"+e);
+		}
+	}
+	@Test
+	public void pruebaNuevoRegistro(){
+		Marcas marcas = new Marcas();
+		System.out.println("\n\nTest nueva marca");
 		try{
 			marcas.setNombreMarca("Asus");
 			marcasService.nuevaMarca(marcas);
+			
 		}catch(Exception e){
-			System.out.println("Error: "+ e);
+			System.out.println("error"+e);
 		}
 	}
-	
-	@Ignore
-	public void eliminar(){
-		Map<String, Integer>mapMarca = new HashMap<>();
-		mapMarca.put("idMarca",6);
+	@Test
+	public void pruebaEliminar(){
+		Map<String,Integer> mapMarca = new HashMap<>();
+		mapMarca.put("IdMarca", 8);
+		System.out.println("\n\n Test eliminar\n ");
 		try{
+			
 			marcasService.eliminarMarca(mapMarca);
-			System.out.println("\n\nTest eliminar");
-			System.out.println(" ");
+			
 		}catch(Exception e){
-			System.out.println("Error:"+ e);
+			System.out.println("Error: "+e);
 		}
 	}
-	
-	@Ignore
-	public void modificar(){
-		System.out.println("\n\nTest modificar");
+	@Test
+	public void pruebaModificar(){
+		System.out.println("\n\n Test modificar");
 		try{
-			marcasService.modificarMarca("Picafresas",5);
+			marcasService.modificarMarca("Picafresas",7);
 			System.out.println(" ");
+			
 		}catch(Exception e){
-			System.out.println("Error:"+ e);
+			System.out.println("error"+e);
 		}
 	}
-	
-	
-	
 }
