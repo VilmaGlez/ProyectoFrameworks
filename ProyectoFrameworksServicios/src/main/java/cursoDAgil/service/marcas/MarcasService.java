b74b3e03bf9d959499fcb3879394dc4300ac9156
TranquilinoHG <83606375+TranquilinoHG@users.noreply.github.com>
2021-05-05 13:10:01 -0500

    Update MarcasService.java

--- ProyectoFrameworksServicios/src/main/java/cursoDAgil/service/marcas/MarcasService.java
+++ ProyectoFrameworksServicios/src/main/java/cursoDAgil/service/marcas/MarcasService.java
@@ -1,14 +1,12 @@
 package cursoDAgil.service.marcas;
-
 import java.util.List;
 import java.util.Map;
 
 import cursoDAgil.bd.domain.Marcas;
-
-public interface MarcasService {
-		List<Marcas> obtenerMarcas();
-		Integer nuevaMarca(Marcas marcas);
-		Marcas obtenerMarcaPorId(Map<String, Integer>mapMarca);
-		Integer eliminarMarca(Map<String, Integer>mapMarca);
-		Integer modificarMarca(String nombreMarca, int idMarca);
+public interface MarcasService{
+	List<Marcas> obtenerMarcas();
+	Integer nuevaMarca(Marcas marcas);
+	Marcas obtenerMarcaPorId(Map<String, Integer> mapMarca);
+	Integer eliminarMarca(Map<String, Integer>mapMarca);
+	Integer modificarMarca(String nombreMarca, int idMarca);
 }
