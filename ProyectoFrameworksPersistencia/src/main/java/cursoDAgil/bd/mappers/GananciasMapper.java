783c4c1a0aa0521dec657c8534dbd14f7b8a8176
Jocelyn502-A <56168256+Jocelyn502-A@users.noreply.github.com>
2021-06-09 21:44:12 -0500

    Add files via upload

---
+++ ProyectoFrameworksPersistencia/src/main/java/cursoDAgil/bd/mappers/GananciasMapper.java
@@ -0,0 +1,24 @@
+package cursoDAgil.bd.mappers;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import cursoDAgil.bd.domain.Ganancias;
+
+public interface GananciasMapper {
+	List<Ganancias> listarTodasGanancias();
+
+	List<Ganancias> obtenerGananciasPorFecha(Date fecha);
+	
+	Ganancias obtenerGananciasPorId(Map<String,Integer> mapGanancias);
+
+	Integer nuevaGanancia(Ganancias ganancias);
+	
+	Integer eliminarGanancia(Integer idGanancia);
+	
+	Integer modificarGanancia(@Param("totalGanancias") double totalGanancias, @Param("idGanancias") Integer idGanancias);
+	
+}
