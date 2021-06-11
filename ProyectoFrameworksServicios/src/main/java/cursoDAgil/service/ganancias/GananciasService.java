64d7abc2de94ab4ba24340df43c6d8306a85d2b0
Jocelyn502-A <56168256+Jocelyn502-A@users.noreply.github.com>
2021-06-11 16:17:19 -0500

    Add files via upload

---
+++ ProyectoFrameworksServicios/src/main/java/cursoDAgil/service/ganancias/GananciasService.java
@@ -0,0 +1,25 @@
+package cursoDAgil.service.ganancias;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import cursoDAgil.bd.domain.Ganancias;
+
+public interface GananciasService {
+	List<Ganancias> listarTodasGanancias();
+
+	List<Ganancias> obtenerGananciasPorFecha(String fecha);
+
+	Ganancias obtenerGananciasPorId(Map<String, Integer> mapGanancias);
+
+	Integer nuevaGanancia(Ganancias ganancias);
+
+	Integer eliminarGanancia(Integer idGanancia);
+
+	Integer modificarGanancia(@Param("totalGanancias") double totalGanancias,
+			@Param("idGanancias") Integer idGanancias);
+
+	double obtenerTotalGanancias();
+}
