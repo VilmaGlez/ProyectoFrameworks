b34167a25731afd19a7fd0266a41e56aaa03d98f
SayraMtz <56168270+SayraMtz@users.noreply.github.com>
2021-06-13 11:58:32 -0500

    Add files via upload

--- PFrameworksPresentacion/src/main/java/cursoDAgil/converterBean/direccion/DireccionConverter.java
+++ PFrameworksPresentacion/src/main/java/cursoDAgil/converterBean/direccion/DireccionConverter.java
@@ -1,43 +1,53 @@
-package cursoDAgil.converterBean.direccion;
+/*
+ * Número de equipo: 4
+ * Autores de la clase:
+ *  Garcia Jocelyn Nohemi
+ *  Martinez Quiroz Sayra
+ *  Fecha: 28/05/2021
+ * 
+ */
+package cursoDAgil.converter;
 
 import java.util.HashMap;
 import java.util.Map;
-import cursoDAgil.bd.domain.Direccion;
+
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import cursoDAgil.service.direccion.DireccionService;
 
+import cursoDAgil.service.direccion.DireccionService;
+import cursoDAgil.bd.domain.Direccion;
 @Named
 public class DireccionConverter implements Converter {
-@Inject
-DireccionService direccionService;
-@Override
-public Object getAsObject(FacesContext context, UIComponent component, String value) {
-	Map<String, Integer>mapDireccion = new HashMap<>();
-	mapDireccion.put("idDireccion",Integer.parseInt(value));
-	System.out.println("preuba2:"+mapDireccion);
-	if (value != null && (value.trim().length() > 0)) {
-	try {
-
-		return direccionService.obtenerDireccionPorId(mapDireccion);
-	} catch (NumberFormatException e) {
-	return null;
-	}
-	} else {
-	return null;
-	}
-}
-@Override
-public String getAsString(FacesContext context, UIComponent component, Object value) {
+	@Inject
+	DireccionService direccionService;
 	
-	if (((value != null) && ((Direccion) value).getIdDireccion()!= null)){
-		return ((Direccion) value).getIdDireccion().toString();
-	} else {
-	return null;
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value){
+		if(value != null && (value.trim().length()>0)){
+			try{
+				Map<String,Integer>mapDireccion =new HashMap<>();
+				mapDireccion.put("idDireccion", Integer.parseInt(value));
+				return direccionService.obtenerDireccionPorId(mapDireccion);
+				
+			}catch(NumberFormatException e){
+				return null;
+			}
+		}else{
+			return null;
+		}
+	}
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value){
+		if(((value !=null)&& ((Direccion)value).getIdDireccion()!=null)){
+			return ((Direccion)value).getIdDireccion().toString();
+		}else
+			return null;
 	}
 }
-}
+
+
